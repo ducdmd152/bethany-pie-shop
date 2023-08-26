a0d97589f1ac5c7916fd5f19a20e31e705ad4f3e
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2023-08-26 21:38:08 +0700

    Data Migrations

--- Program.cs
+++ Program.cs
@@ -1,13 +1,22 @@
 using BethanysPieShop.Models;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
-builder.Services.AddScoped<IPieRepository, MockPieRepository>();
-
-
-
 builder.Services.AddControllersWithViews();
+
+
+//builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+//builder.Services.AddScoped<IPieRepository, MockPieRepository>();
+
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IPieRepository, PieRepository>();
+
+
+builder.Services.AddDbContext<BethanysPieShopDbContext>(options => {
+    options.UseSqlServer(
+        builder.Configuration["ConnectionStrings:BethanysPieShopDbContextConnection"]);
+});
 
 var app = builder.Build();
 
@@ -17,5 +26,7 @@ if (app.Environment.IsDevelopment())
     app.UseDeveloperExceptionPage();
 
 app.MapDefaultControllerRoute();
+
+DbInitializer.Seed(app);
 
 app.Run();
